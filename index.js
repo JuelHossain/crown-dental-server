bb5679f5a96343d46b2f5d45bacc8cfee4ecdbef
juelhossain <juelhossainnn@gmail.com>
2023-02-28 22:37:41 +0600

    project is done

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const client = require("./db/client");
+const { serviceRouter, reviewsRouter, usersRouter } = require("./routers");
+const { getServiceCount } = require("./controller/servicesController");
+const { getReviewCount } = require("./controller/reviewsController");
 
 const app = express();
 const port = process.env.PORT;
@@ -12,6 +15,11 @@ app.use(express.json());
 (async () => {
   try {
     await client.connect();
+    app.use("/users", usersRouter);
+    app.use("/services", serviceRouter);
+    app.use("/reviews", reviewsRouter);
+    app.get("/serviceCount", getServiceCount);
+    app.get("/reviewCount", getReviewCount);
   } catch (err) {
     console.log("There was some error", err);
   } finally {
